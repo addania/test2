7ccdb0118615fd55e0f998e18d0f2e28e330d6af
addania <mirka.schw@gmail.com>
2019-10-03 22:30:02 +0200

    testing video

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,18 @@
 import React from "react"
 import { HeaderComponent } from "../components/header.js";
+import "../styles/global.css";
+import vid from "./vid/ulrik.mp4";
 
 export default () => (
   <div>
     <h1>OMFG IT WORKS!!!!!!!!!!!!!!! YOU ROCK!!!!!</h1>
     <HeaderComponent title="RANDOM TEXT"/>
-  </div>)
+    <video  controls>
+    <source src="https://clips.vorwaerts-gmbh.de/VfE_html5.mp4" type="video/mp4">
+    </source>
+    </video>
+    <video  controls>
+    <source src={vid} type="video/mp4">
+    </source>
+    </video>
+ </div>)
